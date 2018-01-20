99d4eb47c26a53431efe49d9e2edce6f49e3b328
hurui <guovergil@gmail.com>
2018-01-20 22:58:59 +0800

    复数输出

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::fmt;
 
-struct Structure(i32);
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
 
-impl fmt::Display for Structure {
+impl fmt::Display for Complex {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} hello", self.0)
+        write!(f, "{} + {}i", self.real, self.imag)
     }
 }
 
 fn main() {
-    println!("{}",Structure(12));
+    let num = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+    println!("Display: {}", num);
+    println!("Debug: {:?}", num)
 }
